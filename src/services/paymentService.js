db0f0f787d2e704114080c1867532803165adb08
ferjaniahmed <ferjani.ahmed.2001@gmail.com>
2024-07-23 12:44:50 +1300

    fix some models and payment route

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -1,9 +1,13 @@
 import axios from "axios"
 export const createPayment = async (data )=>{
+    const {payment_token , ...rest} = data
+    if(!payment_token){
+        return
+    }
     try{
         const pay = await axios.post("https://sandbox.paymee.tn/api/v2/payments/create" , 
             data , 
-            {headers : {"Content-Type":"application/json" , "Authorization": "Token fae3dbf78030a01f10d7228cb7dfada3c93d9357"}}
+            {headers : {"Content-Type":"application/json" , "Authorization": `Token ${payment_token}`}}
         )
         return pay.data
     }catch(err){
